02bf9e9dbec4c36721d9151f046b176dbdfedb62
Nicolas R <nicolas@atoomic.org>
2020-08-20 16:19:33 -0600

    lib/OverPAN/Client.pm

--- lib/OverPAN/Client.pm
+++ lib/OverPAN/Client.pm
@@ -1,1 +1,215 @@
-1
+package OverPAN::Client;
+
+use OverPAN::std;    # import strict, warnings & features
+
+use OverPAN::Logger; # import all
+use OverPAN::Http;
+
+# need to load all commands to fatpack them
+use OverPAN::Client::cmd::abort   ();
+use OverPAN::Client::cmd::commit  ();
+use OverPAN::Client::cmd::help    ();
+use OverPAN::Client::cmd::patch   ();
+use OverPAN::Client::cmd::version ();
+
+use Cwd ();
+
+use Simple::Accessor qw{
+    name
+    http
+    cwd
+    homedir
+    builddir
+};
+
+use File::Path qw(mkpath rmtree);
+
+use Getopt::Long qw(:config no_auto_abbrev no_ignore_case bundling);
+
+sub build ( $self, %options ) {
+
+    foreach my $k ( sort keys %options ) {
+        $self->{$k} = $options{$k};    # could also use the accessor
+    }
+
+    $self->cwd;                        # setup CWD asap
+
+    # defaults values
+    my $defaults = {
+        check_signature => 1,
+
+        # --with
+        with_requires   => 1,
+        with_recommends => 0,
+        with_suggests   => 0,
+        with_configure  => 0,
+        with_build      => 1,
+        with_test       => 1,
+        with_runtime    => 1,
+        with_develop    => 0,
+    };
+    foreach my $k ( sort %$defaults ) {
+        $self->{$k} //= $defaults->{$k};
+    }
+
+    $self->{features} = [];
+
+    return $self;
+}
+
+sub _build_cwd {
+    return Cwd::cwd();
+}
+
+sub _build_homedir {
+    $ENV{HOME} or die q[HOME environmenet variable not set];
+}
+
+sub _build_builddir($self) {
+    my $path = $self->cache_dir . '/build';
+
+    return $path if -d $path;
+    mkpath($path) or die "fail to create build directory at: $path";
+
+    return $path;
+}
+
+sub DESTROY($self) {
+
+ # use on purpose the hash accessor to avoid creating the directory on DESTROY
+    if ( ref $self && $self->{builddir} && $self->{cleanup} ) {
+        my $dir = $self->{builddir};
+        if ( -d $dir && !-l $dir ) {
+            DEBUG("rmtree .build directory: $dir");
+            File::Path::rmtree($dir);
+        }
+    }
+}
+
+sub _build_http {
+    OverPAN::Http->create;
+}
+
+sub parse_options ( $self, @opts ) {
+    local @ARGV = @opts;
+
+    my $n_tests;
+
+    GetOptions(
+
+        # used
+        "color!" => \( $self->{color} ),
+
+        "v|verbose"      => \( $self->{verbose} ),
+        "d|debug"        => \( $self->{debug} ),
+        "show-progress!" => \( $self->{show_progress} ),
+    ) or exit 1;
+
+    $self->{color} = 1 if !defined $self->{color} && -t STDOUT;
+    if ( !defined $self->{show_progress} && -t STDOUT ) {
+        if ( scalar @ARGV > 1 ) {
+            $self->{show_progress} = 1;    # auto hide progress
+        }
+        else {
+# this is making -v behaves differently when installing/testing a single element
+            $self->{show_progress} = 0;    # do not delete previous lines
+        }
+    }
+
+    $self->{show_progress} = 0 if $self->debug;    # no progress on debug
+    $self->{show_progress} = 0 unless -t STDIN;
+
+    $self->{run_tests} = 0 if $n_tests;            # alias -n for --no-tests
+
+    # debug enable verbose
+    $self->{verbose} = 1 if $self->{debug};
+
+    $self->{cleanup} //= 1;
+
+    $OverPAN::Logger::COLOR         = 1 if $self->{color};
+    $OverPAN::Logger::VERBOSE       = 1 if $self->{verbose};
+    $OverPAN::Logger::DEBUG         = 1 if $self->{debug};
+    $OverPAN::Logger::SHOW_PROGRESS = 1 if $self->{show_progress};
+
+    if ( @ARGV && $ARGV[0] eq "-" ) {
+        $self->{argv}     = $self->read_argv_from_stdin;
+        $self->{cpanfile} = undef;
+    }
+    else {
+        $self->{argv} = \@ARGV;
+    }
+
+    return $self;
+}
+
+sub read_argv_from_stdin {
+    my $self = shift;
+    my @argv;
+    while ( my $line = <STDIN> ) {
+        next if $line !~ /\S/;
+        next if $line =~ /^\s*#/;
+        $line =~ s/^\s*//;
+        $line =~ s/\s*$//;
+        push @argv, split /\s+/, $line;
+    }
+    return \@argv;
+}
+
+sub get_cmd_sub_for ( $self, $cmd ) {
+    return unless defined $cmd;
+
+    $cmd =~ s{^-+}{};
+    $cmd =~ s{-}{}g;    # from-tarball -> fromtarball
+
+    # command aliases
+    my $aliases = {
+        h => 'help',
+        v => 'version',
+        V => 'version',
+        a => 'abort',
+        q => 'abort',
+        c => 'commit',
+        p => 'patch',
+    };
+
+    $cmd = $aliases->{$cmd} if defined $aliases->{$cmd};
+
+    return unless $cmd =~ m{^[A-Za-z0-9_]+$};
+    return "OverPAN::cmd::$cmd"->can('run');
+}
+
+sub run ( $self, @argv ) {
+    my $cmd = '';
+
+    my $default_action = 'patch';
+
+    my $run;
+    if ( scalar @argv ) {
+        if ( $run = $self->get_cmd_sub_for( $argv[0] ) ) {
+            $cmd = shift @argv;
+        }
+        else {
+            $run = $self->get_cmd_sub_for($default_action);
+            $cmd = $default_action;
+        }
+    }
+    else {
+        $run = $self->get_cmd_sub_for('help');
+        $cmd = 'help';
+    }
+
+    die qq[Unknown subcommand '$cmd']
+        unless defined $run && ref $run eq 'CODE';
+
+    $self->parse_options(@argv);
+
+    $cmd =~ s{^-+}{} if $cmd;
+    ## maybe do an extra parse_options for every commands?
+    if ( $cmd && $cmd !~ m{^(?:help|version)$} ) {
+        INFO("Running action '$cmd'");
+    }
+
+    return $run->( $self, @{ $self->{argv} } );
+}
+
+1;
