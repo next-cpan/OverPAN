b83ddba4cf2567a4977eb14213395636425f4d01
Nicolas R <nicolas@atoomic.org>
2020-08-24 10:45:48 -0600

    Move vendor to lib/OverPAN/vendor

--- lib/OverPAN.pm
+++ lib/OverPAN.pm
@@ -2,6 +2,13 @@ package OverPAN;
 
 use OverPAN::std;
 
+BEGIN {
+    use FindBin;
+    my $vendor = $INC{'OverPAN.pm'} or die;
+    $vendor =~ s{\.pm$}{/vendor};
+    unshift @INC, $vendor if -d $vendor;
+}
+
 our $VERSION  = "0.0001";
 
 1;
