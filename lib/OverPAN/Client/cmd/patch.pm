5f75316776934a2e96a691b296d33160cb9ba00d
Nicolas R <nicolas@atoomic.org>
2020-08-24 17:20:34 -0600

    cleanup

--- lib/OverPAN/Client/cmd/patch.pm
+++ lib/OverPAN/Client/cmd/patch.pm
@@ -54,28 +54,7 @@ EOS
 
     chdir( $self->cwd );
 
-    # return 1 unless scalar @modules;
-
-    # my $installer = App::cnext::Installer->new( cli => $self );
-
-    # foreach my $module (@modules) {
-    #     if ( $module eq '.' ) {
-    #         INFO "Installing distribution from .";
-    #         if ( !$installer->install_from_file() ) {
-    #             FAIL "Fail to install distribution from .";
-    #             return 1;
-    #         }
-    #         next;
-    #     }
-
-    #     INFO("Looking for module: $module");
-    #     if ( !$installer->install_single_module_or_repository($module) ) {
-    #         FAIL("Fail to install $module or its dependencies.");
-    #         return 1;
-    #     }
-    # }
-
-    DONE("patch succeeds");
+    #DONE("patch succeeds");
 
     return 0;
 }
